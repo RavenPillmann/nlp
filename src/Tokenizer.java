f3d26df0a2211222b618511b4db15eb116f50921
Raven Pillmann <pillmannr@gmail.com>
2017-04-18 21:54:10 -0500

    tokenizing

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,15 +1,26 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tokenizer {
 	private String inputString;
-	private ArrayList tokens;
+	private ArrayList<String> tokens;
 
 	public Tokenizer(String inputString) {
 		this.inputString = inputString;
 	}
 
 	public void tokenize() {
+		Pattern tokenPattern = Pattern.compile("((\\w)+'{0,1}(\\w)+)|[(\\.),(\\?)!:;\"']");
+		this.tokens = new ArrayList<String>();
+
+		Matcher tokenMatcher = tokenPattern.matcher(this.inputString);
+
+		while (tokenMatcher.find()) {
+			this.tokens.add(tokenMatcher.group());
+		}
+
 		// Tokenize
 		// Regex should separate out words as well as punctuation, but not punctuation the is contained within words.
 		// Also, numbers
@@ -24,9 +35,12 @@ public class Tokenizer {
 
 		System.out.println("Enter a phrase to parse");
 
-		String phrase = String.format("Your phrase was: %s", sc.nextLine());
+		String input = sc.nextLine();
 
-		System.out.println(phrase);
+		Tokenizer tokenizer = new Tokenizer(input);
+		tokenizer.tokenize();
+
+		System.out.println(tokenizer.tokens);
 
 		sc.close();
 	}
